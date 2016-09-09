6dd8031372d2d0c0e134cfc4770f2c5a3f9bc7c4
J. R. Okajima <hooanon05g@gmail.com>
2016-09-09 12:33:21 +0900

    aufs4.5 20160912
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- include/uapi/linux/aufs_type.h
+++ include/uapi/linux/aufs_type.h
@@ -39,7 +39,7 @@
 
 #include <linux/limits.h>
 
-#define AUFS_VERSION	"4.5-20160905"
+#define AUFS_VERSION	"4.5-20160912"
 
 /* todo? move this to linux-2.6.19/include/magic.h */
 #define AUFS_SUPER_MAGIC	('a' << 24 | 'u' << 16 | 'f' << 8 | 's')
