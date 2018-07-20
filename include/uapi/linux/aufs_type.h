b01cf48b67e61ff30ff98b8488c904369a49286d
J. R. Okajima <hooanon05g@gmail.com>
2018-07-20 13:19:39 +0900

    aufs4.9 20180723
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- include/uapi/linux/aufs_type.h
+++ include/uapi/linux/aufs_type.h
@@ -39,7 +39,7 @@
 
 #include <linux/limits.h>
 
-#define AUFS_VERSION	"4.9-20180716"
+#define AUFS_VERSION	"4.9-20180723"
 
 /* todo? move this to linux-2.6.19/include/magic.h */
 #define AUFS_SUPER_MAGIC	('a' << 24 | 'u' << 16 | 'f' << 8 | 's')
